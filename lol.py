365e96715d216e295af2a443949a5948dbbb2839
Julius Buset Asplin <julius_asplin@hotmail.com>
2013-01-31 14:35:19 +0100

    lol

---
+++ lol.py
@@ -0,0 +1,19 @@
+'''
+Created on 31. jan. 2013
+
+@author: juliusbuset
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, selfparams):
+        '''
+        Constructor
+        '''
+        
+        def loler(self):
+            
